1e01d6d971e9af9fe40dcd8ad792312defe0d31f
Matt Perkins <hello@mattperkins.me>
2018-09-03 08:20:06 -0400

    add gh-pages and deploy

--- rebuild.sh
+++ rebuild.sh
@@ -1,4 +1,6 @@
 docker-compose -f docker-compose-dev.yml down
 docker-compose -f docker-compose-build.yml down
+docker-compose -f docker-compose-deploy.yml down
 docker-compose -f docker-compose-dev.yml build
 docker-compose -f docker-compose-build.yml build
+docker-compose -f docker-compose-deploy.yml build
